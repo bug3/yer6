280ea20e7aac8cd5816b8570b3c7c38ed4ab9986
bug3 <bug3dev@gmail.com>
2022-04-17 02:27:28 +0300

    Add hold package

--- scripting/packages.sh
+++ scripting/packages.sh
@@ -5,3 +5,9 @@ dpkg -s $package | grep Version
 
 # List installed packages
 grep "apt install " /var/log/apt/history.log | awk '{print $4}'
+
+# Hold package from upgrade
+sudo apt-mark hold $package
+
+# List packages on hold
+sudo dpkg --get-selections | grep "hold"
