8f1ccdaf3b9bb2944e8b3109180639f605d87207
bug3 <bug3dev@gmail.com>
2022-04-18 23:40:48 +0300

    Remove files

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,5 +6,5 @@ currentDir=$(dirname $(realpath ${BASH_SOURCE[0]}))
 if [[ $# -eq 0 ]]; then
     find $currentDir -type f -executable -exec cp {} $destDir \;
 elif [[ $1 == "-r" ]]; then
-    echo "remove"
+    find $currentDir -type f -executable | xargs -I{} basename {} | xargs -I{} rm $destDir/{}
 fi
