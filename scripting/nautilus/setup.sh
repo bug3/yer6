ecccabd2388b95d5873608b7fc3e4b4403481cb0
bug3 <bug3dev@gmail.com>
2022-04-19 00:14:02 +0300

    Add remove option

--- scripting/nautilus/setup.sh
+++ scripting/nautilus/setup.sh
@@ -5,4 +5,8 @@
 destDir="$HOME/.local/share/nautilus/scripts"
 currentDir=$(dirname $(realpath ${BASH_SOURCE[0]}))
 
-find $currentDir -type f -executable -exec cp {} $destDir \;
+if [[ $# -eq 0 ]]; then
+    find $currentDir -type f -executable -exec cp {} $destDir \;
+elif [[ $1 == "-r" ]]; then
+    find $currentDir -type f -executable | xargs -I{} basename {} | xargs -I{} rm $destDir/{}
+fi
