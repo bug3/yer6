586b91af581c3140ea9bff2b3968c48b6eebc194
bug3 <bug3dev@gmail.com>
2022-04-18 22:57:05 +0300

    Copy files

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+destDir="$HOME/.local/bin"
+currentDir=$(dirname $(realpath ${BASH_SOURCE[0]}))
+
+if [[ $# -eq 0 ]]; then
+    find $currentDir -type f -executable -exec cp {} $destDir \;
+elif [[ $1 == "-r" ]]; then
+    echo "remove"
+fi
