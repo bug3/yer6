85d862aea714c7fdd2e19d024babb4f399bc069c
bug3 <bug3dev@gmail.com>
2022-04-19 00:11:16 +0300

    Change var name

--- scripting/nautilus/setup.sh
+++ scripting/nautilus/setup.sh
@@ -2,7 +2,7 @@
 
 # Source: https://help.ubuntu.com/community/NautilusScriptsHowto
 
-nautScripts="$HOME/.local/share/nautilus/scripts"
+destDir="$HOME/.local/share/nautilus/scripts"
 currentDir=$(dirname $(realpath ${BASH_SOURCE[0]}))
 
-find $currentDir -type f -executable -exec cp {} $nautScripts \;
+find $currentDir -type f -executable -exec cp {} $destDir \;
